2313a6463db76db984b849b84fe7eaaab20ebe23
philipp.schaer@gmail.com <philipp.schaer@gmail.com@5fe94471-ae1b-622a-9afa-d5785f8811c6>
2012-05-25 15:30:01 +0000

    Added two scripts that extract some basic information on the relevance assessments

---
+++ girt-scripts/extractRelevanceCounts.groovy
@@ -0,0 +1,41 @@
+def startTime = System.currentTimeMillis()
+
+// Put path to GIRT qrels files here
+def qrels = "C:\\evaldata\\girt\\qrels"
+def fileList = new File(qrels).list().toList()
+def output = new File("C:\\evaldata\\results\\GIRT-relevance.txt")
+
+int totalRelevanceCounter = 0
+def relevanceMap = [:]
+def nonRelevanceMap = [:]
+
+fileList.each {file ->
+    def qrel = new File(qrels, file)
+    qrel.splitEachLine(" ") {topic, version, docid, relevance ->
+
+        totalRelevanceCounter++
+
+        if (!relevanceMap[(topic)]) {relevanceMap[(topic)] = 0}
+        if (!nonRelevanceMap[(topic)]) {nonRelevanceMap[(topic)] = 0}
+
+        if (relevance == "1") {
+            relevanceMap[(topic)] = relevanceMap[(topic)] + 1
+        }
+        if (relevance == "0") {
+            nonRelevanceMap[(topic)] = nonRelevanceMap[(topic)] + 1
+        }
+
+    }
+}
+
+println "Extraction duration in total: ${(System.currentTimeMillis() - startTime) / 1000} sec"
+
+println "Total relevance assessments: $totalRelevanceCounter"
+println "Relevance count per topic: $relevanceMap"
+println "Non-Relevance count per topic: $nonRelevanceMap"
+
+output << "topic;rel;nrel\n"
+
+relevanceMap.each {topic, count ->
+    output << "$topic;${relevanceMap[(topic)]};${nonRelevanceMap[(topic)]}\n"
+}
